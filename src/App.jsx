4c7dd0fc9fd5dd01c97075bc43563ad8377d3fe0
CasManny <chukwusabastine2018@gmail.com>
2024-11-04 22:25:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./sections/header";
+import Hero from "./sections/hero";
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
