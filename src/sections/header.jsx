e774b20b693b8596ce206fb775c0a66f99191b8f
CasManny <chukwusabastine2018@gmail.com>
2024-11-06 01:06:54 +0100

    implement xora landing page

--- src/sections/header.jsx
+++ src/sections/header.jsx
@@ -1,17 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 import clsx from "clsx";
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase max-lg:my-4 max-lg:h5 transition-colors duration-500 cursor-pointer hover:text-p1">
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScroll, setHasScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScroll(window.scrollY > 32);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase max-lg:my-4 max-lg:h5 transition-colors duration-500 cursor-pointer hover:text-p1"
+    >
+      {title}
+    </LinkScroll>
+  );
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScroll && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5 ">
         <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
@@ -23,7 +47,7 @@ const Header = () => {
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
-          <div className="w-full border-2 max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+          <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
